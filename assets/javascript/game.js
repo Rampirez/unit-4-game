4879096525d2240ddf103c6763aa492445c1bb4d
Mathew Ramirez <Mathew.J.Ramirez@gmail.com>
2019-10-19 14:51:13 -0500

    Added another function to get the number value for buttons

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,16 @@
 var compNum = 0;
 var userNum = 0;
+var buttonNum = 0;
+var wins = 0;
+var losses = 0;
 
 function compNumSelect() {
     compNum = Math.floor(Math.random() * 101) + 20;
     return compNum
 }
 
-//start game
-    //set random number
-    //check for clicks and add numbers to user number
+function buttonNumSelect() {
+    buttonNum = Math.floor(Math.random * 12) + 1;
+    return compNum
+}
+
