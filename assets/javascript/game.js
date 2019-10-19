5514cb8bf8b2922c3f03577613474109a58715d4
Mathew Ramirez <Mathew.J.Ramirez@gmail.com>
2019-10-19 13:22:17 -0500

    added some boilerplate code to display a random number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,5 +2,8 @@ var compNum = 0;
 var userNum = 0;
 
 function compNumSelect() {
-    compNum = Math.floor(Math.random * 101) + 20;
+    compNum = Math.floor(Math.random() * 101) + 20;
+    return compNum
 }
+
+$("#computer-number").text(compNumSelect());
